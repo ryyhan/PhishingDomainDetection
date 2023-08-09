9a47b31d48ace4ad24d4fd0deedf7e6676652f67
ryyhan <dayel.rehan@gmail.com>
2023-08-09 18:51:20 +0530

    prediction file

---
+++ prediction.py
@@ -0,0 +1,10 @@
+import pickle
+from preprocess import prepare_data
+
+with open("PhishingDomainDetection.pkl", "rb") as file:
+    classifier = pickle.load(file)
+
+
+def predictor(URL):
+    result=classifier.predict(prepare_data(URL))
+    return result[0]
