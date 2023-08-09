711b19abf9ac53c43e7b761167153a74e2ea6bee
ryyhan <dayel.rehan@gmail.com>
2023-08-10 00:14:23 +0530

    updated

--- preprocess.py
+++ preprocess.py
@@ -6,17 +6,18 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 
-#Tokenization, Stemming, Vectorization
-tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+# Tokenization, Stemming, Vectorization
+tokenizer = RegexpTokenizer(r"[A-Za-z]+")
 stemmer = SnowballStemmer("english")
 cv = CountVectorizer()
 
 with open("vectorizer.pkl", "rb") as file:
     cvectorizer = pickle.load(file)
 
-def prepare_data(text) :
+
+def prepare_data(text):
     tokens = tokenizer.tokenize(text)
     stemmed_tokens = [stemmer.stem(token) for token in tokens]
-    stemmed_text = ' '.join(stemmed_tokens)
+    stemmed_text = " ".join(stemmed_tokens)
     vectorized_text = cvectorizer.transform([text])
     return vectorized_text
