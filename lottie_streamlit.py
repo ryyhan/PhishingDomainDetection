cf96b0f0d3fbf33074f2fb350b0dc6621d4dea99
ryyhan <dayel.rehan@gmail.com>
2023-08-09 15:41:40 +0530

    lottie animation added

---
+++ lottie_streamlit.py
@@ -0,0 +1,9 @@
+import requests
+from streamlit_lottie import st_lottie
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    lottie_hello = r.json()
+    st_lottie(lottie_hello)
