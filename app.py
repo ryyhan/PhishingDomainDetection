76020c48d640bab4189eccdcf96b193b5e89ec7c
ryyhan <dayel.rehan@gmail.com>
2023-08-09 15:42:08 +0530

    lottie animation added

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 #import statements
 import streamlit as st
-import requests
-from streamlit_lottie import st_lottie
-
-
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+from lottie_streamlit import load_lottieurl
 
 
 def main():
-    lottie_hello = load_lottieurl("https://lottie.host/052653a2-761d-49c0-8447-fea767592952/NrhCF0XZyW.json")
-
     st.title("Phishing Domain and Malicious URL Detection")
-    st_lottie(lottie_hello)
+    load_lottieurl("https://lottie.host/052653a2-761d-49c0-8447-fea767592952/NrhCF0XZyW.json")
 
 
 if __name__=="__main__":
